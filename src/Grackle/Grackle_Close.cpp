2d271053ae1d4fe342841cfc16270b76f27fbe45
Inhyeok-Song <ihsong@gapp.nthu.edu.tw>
2026-09-10 19:45:14 +0800

    move calculating Pres in DE_ENPY

--- src/Grackle/Grackle_Close.cpp
+++ src/Grackle/Grackle_Close.cpp
@@ -135,8 +135,8 @@ void Grackle_Close( const int lv, const int SaveSg, const real_che h_Che_Array[]
 //          update the dual-energy variable to be consistent with the updated pressure
 #           ifdef DUAL_ENERGY
 //          DUAL_ENERGY only works with EOS_GAMMA, which does not involve passive scalars
-            Pres = EoS_DensEint2Pres_CPUPtr( Dens, Eint, NULL, EoS_AuxArray_Flt, EoS_AuxArray_Int, h_EoS_Table );
 #           if   ( DUAL_ENERGY == DE_ENPY )
+            Pres = EoS_DensEint2Pres_CPUPtr( Dens, Eint, NULL, EoS_AuxArray_Flt, EoS_AuxArray_Int, h_EoS_Table );
             *( fluid[DUAL     ][0][0] + idx_p ) = Hydro_DensPres2Dual( Dens, Pres, EoS_AuxArray_Flt[1] );
 #           elif ( DUAL_ENERGY == DE_EINT )
             *( fluid[DUAL     ][0][0] + idx_p ) = Eint;
